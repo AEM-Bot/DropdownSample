01680cc8a21b2da9e9ed2275f4c0f850f3073da2
schintam <schintam@victoria.com>
2024-08-22 16:45:53 -0400

    api test

--- src/components/MboxValueList.tsx
+++ src/components/MboxValueList.tsx
@@ -38,7 +38,7 @@ const Mboxes: React.FC = () => {
     const fetchMboxes = async () => {
       try {
         // Fetch mboxes from local backend API
-        const dataResponse = await fetch('https://apitest.victoriassecret.com/cmsattributedisplay/v2/category', {
+        const dataResponse = await fetch('https://apitest.victoriassecret.com/cmsmboxes/v1/mboxes', {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
